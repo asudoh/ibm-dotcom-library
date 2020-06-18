be39b6ce0683c398c6cf7c755ec66bd22d93abc7
Akira Sudoh <asudoh@gmail.com>
2020-06-18 11:03:31 +0900

    chore(packages): add elements package
    This change introduces a variant of `@carbon/ibmdotcom-vanilla` that
    uses the latest web standards, which are, Shadow DOM and Custom
    Elements. The version number is set as "under development" for now.
    
    Refs #2810.

---
+++ packages/elements/README.md
@@ -0,0 +1,29 @@
+A Carbon Design System variant that's as easy to use as native HTML elements, with no framework tax, no framework silo.
+
+# `@carbon/ibmdotcom-elements`
+
+`@carbon/ibmdotcom-elements` is a variant of ibm.com Design System with Custom Elements v1 and Shadow DOM v1 specs.
+
+<!-- START doctoc generated TOC please keep comment here to allow auto update -->
+<!-- DON'T EDIT THIS SECTION, INSTEAD RE-RUN doctoc TO UPDATE -->
+
+- [Getting started with development](#getting-started-with-development)
+- [Browser support](#browser-support)
+- [Coding conventions](#coding-conventions)
+
+<!-- END doctoc generated TOC please keep comment here to allow auto update -->
+
+## Getting started with development
+
+1. Fork this repository and clone it
+2. `yarn install`
+3. `yarn storybook`
+
+## Browser support
+
+- Latest Chrome/Safari/FF ESR
+- IE and classic Edge are _not_ supported
+
+## Coding conventions
+
+Can be found at [here](./docs/coding-conventions.md).
---
+++ packages/elements/src/components/link-with-icon/link-with-icon-story.ts
@@ -0,0 +1,38 @@
+/**
+ * @license
+ *
+ * Copyright IBM Corp. 2020
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { html } from 'lit-element';
+import { action } from '@storybook/addon-actions';
+import { boolean } from '@storybook/addon-knobs';
+import ArrowRight20 from 'carbon-custom-elements/es//icons/arrow--right/20';
+import ifNonNull from 'carbon-custom-elements/es/globals/directives/if-non-null';
+import textNullable from '../../../.storybook/knob-text-nullable';
+import './link-with-icon';
+
+export const Default = ({ parameters }) => {
+  const { disabled, href, onClick } = parameters?.props?.['dds-link-with-icon'] ?? {};
+  return html`
+    <dds-link-with-icon ?disabled="${disabled}" href="${ifNonNull(href)}" @click="${onClick}">
+      Link text${ArrowRight20({ slot: 'icon' })}
+    </dds-link-with-icon>
+  `;
+};
+
+export default {
+  title: 'Components/Link with icon',
+  parameters: {
+    knobs: {
+      'dds-link-with-icon': ({ groupId }) => ({
+        disabled: boolean('Disabled (disabled)', false, groupId),
+        href: textNullable('Link href (href)', 'https://github.com/carbon-design-system/carbon-custom-elements', groupId),
+        onClick: action('click'),
+      }),
+    },
+  },
+};
